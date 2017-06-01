24f97e4816d83ef204408529bb0bc17e6098f1dd
Ksenia <ksbazh@mail.ru>
2017-06-01 17:41:28 +0400

    test_1

--- application/models/topic/ex1.php
+++ application/models/topic/ex1.php
@@ -5,23 +5,17 @@ class Ex1 extends CI_Model {
 
 	function numex1()
 	{
-		echo "<div style = 'text-align: center;'>
-				<form method = 'post'>
-					<h1>Вычисление корней квадратного уравнения</h1>
-					<h3>Введите данные</h3>
+		echo "<form method = 'post'>
 					<p><input type = 'text' name = 'a' style='width: 70'> x^2 +
 					<input type = 'text' name = 'b' style='width: 70'> x +
 					<input type = 'text' name = 'c' style='width: 70'>
-					<p><input type='hidden' name='function_start' value='go'></p>
 					<p><input type = 'submit' value = 'Отправить'></p>
-				</form>
-			</div>";
+				</form>";
 			
 		function res()
 		{
 			if(isset($_POST['a'])) {$a = $_POST['a'];};
-			if(isset($_POST['b'])) {$b = $_POST['b'];};
-			if(isset($_POST['c'])) {$c = $_POST['c'];};
+			
 			
 			if (($a !=null) && ($b!=null) && ($c!=null))
 			{
